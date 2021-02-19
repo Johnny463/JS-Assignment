c80c1894b5aa8f3248734f3612c509f60644e981
Johnny463 <70976999+Johnny463@users.noreply.github.com>
2021-02-19 05:43:30 -0800

    Add files via upload

---
+++ Chapter - 9/index.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter your First name", "Muhammad");
+var lastName = prompt("Enter your last Name", "Junaid");
+var age = Number(prompt("Enter your age"));
+
+alert("Hello " +firstName+ " " +lastName+", " + "You are " +age+ " years old");
